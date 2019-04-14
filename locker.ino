95aabf685f088fa032ef449b2ae98e145d9f2131
hannasd <45179778+hannasd@users.noreply.github.com>
2019-04-14 11:24:36 +0430

    Add files via upload
    First Commit.

--- locker.ino
+++ locker.ino
@@ -6,23 +6,29 @@
 #include <Adafruit_SPITFT.h>
 #include <Adafruit_SPITFT_Macros.h>
 #include <gfxfont.h>
-
+#include <time.h>
 #include <Adafruit_SSD1306.h>
-//#include <ArduinoJson.h>
+#include <ArduinoJson.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
-
-Adafruit_SSD1306 display(128,64,&Wire,D3);
- uint8_t amirali[]={0x79,0x6,0xF,0x1A};
+#define amirali 0
+#define hanna 1
+#define aghaye_asadi_rahmani 2
+unsigned long timenow = 0;
 PN532_SPI pn532spi(SPI, D4);
-//PN532_I2C pn532i2c(Wire);
 PN532 nfc(pn532spi);
+Adafruit_SSD1306 display(128,64,&Wire,D3);
+// uint8_t amirali[]={0x79,0x6,0xF,0x1A};
+// uint8_t hanna[] = {0xCD,0x77,0x6A,0xA};
+//PN532_I2C pn532i2c(Wire);
+uint8_t eepromLoc = 0;
 uint8_t numberOfCards;
-uint8_t cards[128][4];
+uint8_t cards[128][4] = {{0x79,0x6,0xF,0x1A} , {0xCD,0x77,0x6A,0xA}};
 //const int capacity = 50 * JSON_OBJECT_SIZE(1);
-
 HTTPClient http;
-uint32_t uidAsInteger(uint8_t uid[]) {
+
+
+unsigned int uidAsInteger(uint8_t uid[]) {
   int result = 0;
   for (int i = 0 ; i < 4; i++) {
     result += (uid[4 - i - 1] * (1 << (4 * 2 * i)));
@@ -30,63 +36,66 @@ uint32_t uidAsInteger(uint8_t uid[]) {
   return result;
 }
 
-void saveToEEPROM(uint8_t uid[]) {
-  for (uint8_t i = 0 ; i < 4; i++) {
-    EEPROM.write((numberOfCards * 4) + i , uid[i]);
-  }
-  numberOfCards += 1;
-  EEPROM.write(511 , numberOfCards);
-
+void saveToEEPROM(JsonArray repos ) {
+    numberOfCards = repos.size();
+   for(int i = 0 ; i < repos.size() ; i++){
+    EEPROM.write(i ,   uidAsInteger(repos[i]));
+   }
+  EEPROM.write(512 , numberOfCards);
+  return;
 }
 
-bool isMember(uint8_t uid[]) {
-  for (int i = 0 ; i < numberOfCards ; i++) {
-    for (int j = 0; j < 4; j++) {
-      if (cards[i][j] != uid[j]) {
-        break;
-      }
-      if (j == 3) {
-        return true;
-      }
+
+void POSTLog(unsigned int uid  , bool accepted ){
+
+   DynamicJsonDocument doc(1024);
+   
+  doc["uid"] = uid;
+  doc["authented"] = accepted; 
+  serializeJson(doc, Serial);
+  JsonArray logs = doc["uid"]["authented"];
+    http.begin("");
+    
+    http.POST(logs[0]);
+  }
+
+bool isMember(unsigned int uid)
+{
+  for(int i = 0 ; i < EEPROM.read(512) ; i++){
+    if(EEPROM.read(i) == uid ){
+     return true;
     }
   }
   return false;
 }
 
-//void getListFromServer(){
-//  http.begin("http://arminmaz.pythonanywhere.com/core/update/");
-//  int httpCode = http.GET();
-//  if(httpCode < 0){
-//    Serial.printf("Status :::: [HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-//  }
-//  Stream& response = http.getStream();
-//  DynamicJsonDocument doc(2048);
-//
-////  deserializerJson(doc, response);
-//
-//  JsonArray repos = doc["uid"];
-//
-//  for(JsonObject repo : repos){
-//    Serial.print(" _ ");
-//    Serial.print(repo["uid"].as<char* >());
-////      repo =  std::stoi (repos,NULL,16);
-//  }
-//  String s = http.getString();
-//
-//  char json[50];
-//  strcpy(json,s.c_str());
-//  deserializeJson(doc, json);
-//  JsonArray arr = doc.as<JsonArray>();
-//
 
-//
-//void saveList(JsonArray repos) {
-//  for (JsonObject repo : repos) {
-//    int i = 0;
-//    EEPROM.put(0 , repo);
-//    EEPROM.commit();
-//  }
-//}
+ void getListFromServer(){
+  http.begin("http://arminmaz.pythonanywhere.com/core/update/");
+  int httpCode = http.GET();
+  if(httpCode < 0){
+    Serial.printf("Status :::: [HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  } 
+  Stream& response = http.getStream();
+  DynamicJsonDocument doc(2048);
+  deserializeJson(doc, response);
+  JsonArray repos = doc["uid"];
+
+  for(JsonObject repo : repos){
+    Serial.print(" _ ");
+    Serial.print(repo["uid"].as<char* >());
+     // repo =  stoi (repo["uid"],NULL,10);
+    // repo = (int) strtol(repos["uid"], (char **)NULL, 10);
+
+  }
+  String s = http.getString();
+
+  char json[50];
+  strcpy(json,s.c_str());
+  deserializeJson(doc, json);
+  JsonArray arr = doc.as<JsonArray>();
+  saveToEEPROM(repos);
+ }
 
 void checkForUpdate() {
   http.begin("http://arminmaz.pythonanywhere.com/core/update/");
@@ -98,11 +107,10 @@ void setup(void) {
   EEPROM.begin(513);
   Serial.begin(115200);
   Serial.println("Hello!");
-  EEPROM.write(512, 0);
-  // set up the LCD's number of columns and rows:
+  getListFromServer();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);
   display.clearDisplay();
-  display.setTextSize(1);
+  display.setTextSize(2);
   display.setTextColor(WHITE);
   display.setCursor(0,0);
   display.print("Hello");
@@ -110,11 +118,14 @@ void setup(void) {
 
   nfc.begin();
 
-  
-
+  timenow = millis();
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (! versiondata) {
     Serial.print("Didn't find PN53x board");
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setCursor(1 , 1);
+    display.print("board connection failed!");
     while (1); // halt
   }
 
@@ -136,15 +147,13 @@ void setup(void) {
 
 void loop(void) {
   boolean success;
-  // int id = 0;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
-
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  boolean accepted = false;
+  int uidInt;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  
+  uint8_t uidLength;                        
+  
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
-
+  //harsi sanie yebar post kone list begire
   if (success) {
     Serial.println("Found a card!");
     Serial.print("UID Length: "); Serial.print(uidLength, DEC); Serial.println(" bytes");
@@ -154,41 +163,32 @@ void loop(void) {
       Serial.print(" 0x"); Serial.print(uid[i], HEX);
 
     }
-    for(int i = 0 ; i < 4 ; i++){
-    if(uid[i] == amirali[i] ){
-      if(i == 3){
-       display.clearDisplay();
-      display.setCursor(0 , 0);
-      display.print("welcom amilaliiee");
-      display.display();
-      }
+    uidInt = uidAsInteger(uid);
+    if(millis() - timenow > 30000 ){
+      getListFromServer();
+       timenow = millis();
     }
-      else if(amirali[i]!=uid[i]){
-      display.clearDisplay();
-      display.setCursor(0 , 0);
-      display.print("Authentication failed!");
-       display.display();
-      }
+    //check she doroste ya na
+    if(isMember(uidInt)){
+      // unlock();
+       //age dorost bud log befreste
+       POSTLog(uidInt , !accepted);
     }
-  
- 
-//    display.clearDisplay();
-//    display.setCursor(0,0);
-//    display.print(uidAsInteger(uid));
-//    display.display();
-//    Serial.println("id as int:");
-//    //    Serial.println(uid);
-//    Serial.println("");
-
-}
-    // wait until the card is taken away
+    else
+      POSTLog(uidInt , accepted);
     
-    //send uid to server and get response and savedata
   
+    while (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength)) {}
+    }
+
   else
   {
     // PN532 probably timed out waiting for a card
     Serial.println("Timed out waiting for a card");
   }
-  while (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength)) {}
+   display.clearDisplay();
+  display.print("hello");
+  display.display();
 }
+   
+ 
